09cbe8136d535db36c8397e78752cafd3aab10bc
Damian Golaq <damian.kian@web.de>
2024-03-21 12:24:36 +0100

    2024_03_14

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,17 @@
 package src;
 
+import javax.swing.*;
+
 public class GUI extends JFrame {
+
+    private JSlider slider1;
+    private JButton runButton;
+    private JButton stopButton;
+    private JButton stepButton;
+    private JButton resetButton;
+    private JTextPane LSTTextPane;
+    private JTabbedPane tabbedPane1;
+    private JTable table1;
 
     public GUI() {
 
