8ffca73cc3c5cfa618010eb7d4840e1a6a093874
Damian Golaq <damian.kian@web.de>
2024-03-21 12:49:28 +0100

    2024_03_14

--- src/GUI.java
+++ src/GUI.java
@@ -19,6 +19,8 @@ public class GUI extends JFrame {
     private JPanel mainPanel;
 
     public GUI() {
-
+        setSize(1200, 800);
+        setVisible(true);
+        setContentPane(mainPanel);
     }
 }
